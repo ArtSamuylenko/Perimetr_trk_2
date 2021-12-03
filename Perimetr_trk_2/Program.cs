72b18bb3716a22548ecffdf8a3ed6a82589f5cb0
ArtSamuylenko <89385274775@mail.ru>
2021-12-04 01:44:13 +0300

    Добавьте файлы проекта.

---
+++ Perimetr_trk_2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perimetr_Treugolnika_n2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+
+            {
+                Console.WriteLine("Введите 2 катета и гипотенузу треугольника: ");
+                int storona_1 = int.Parse(Console.ReadLine());
+                int storona_2 = int.Parse(Console.ReadLine());
+                int storona_3 = int.Parse(Console.ReadLine());
+                Perimetr(storona_1, storona_2, storona_3);
+            }
+        }
+        static void Perimetr(int a, int b, int c)
+        {
+
+            int perimetr = a + b + c;
+            Console.WriteLine($"Периметр = {perimetr}");
+            Console.ReadLine();
+        }
+
+    }
+}
